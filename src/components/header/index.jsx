1728760c20e6b0f868dabdecedf753c57f497a62
AmanJain18 <89217032+AmanJain18@users.noreply.github.com>
2023-04-15 15:58:57 +0530

    Logo Update

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -6,7 +6,7 @@ import { VscChromeClose } from "react-icons/vsc";
 import { useNavigate, useLocation } from "react-router-dom";
 
 import ContentWrapper from "../contentWrapper/ContentWrapper";
-import logo from "../../assets/movix-logo.svg";
+import logo from "../../assets/moviemaniax.svg";
 
 const index = () => {
   const [show, setShow] = useState("top");
